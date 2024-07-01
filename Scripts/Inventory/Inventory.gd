d53f67a5dbcc19ba226a6ee57e4aeb438bc93760
toine <21050503@student.hhs.nl>
2024-07-01 23:37:28 +0200

    Inventory system and pick up

---
+++ Scripts/Inventory/Inventory.gd
@@ -0,0 +1,22 @@
+extends Resource
+
+
+class_name Inv
+
+signal update 
+
+@export var Slots: Array[InvSlot]
+
+func Insert (item: InvItem):
+	var itemslots = Slots.filter(func(slot): return slot.item == item)
+	if !itemslots.is.empty():
+		itemslots[0].amount += 1
+	else:
+		var emptyslots = Slots.filter(func(slot): return slot.item == null)
+		if!emptyslots.is_empty():
+			emptyslots[0].item = item
+			emptyslots[0].amount = 1
+	update.emit()
+	
+	
+#Add to player later
---
+++ Scripts/Inventory/MoonPickUp.gd
@@ -0,0 +1,13 @@
+extends StaticBody2D
+@export var item: InvItem
+var player = null 
+
+func _on_area_2d_body_entered(body):
+	if body.has_method("player"):
+		player = body
+		PlayerCollect()
+		await  get_tree().create_timer(0.1).timeout
+		self.queue_free()
+		
+func PlayerCollect():
+	player.collect(item)
