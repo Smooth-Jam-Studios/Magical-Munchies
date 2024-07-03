1d41758e668f96c711c32c5fac0a7aeabbd17aad
Justwouter <wouter2312@gmail.com>
2024-07-03 12:15:13 +0200

    Made a single pickup, reordered root scene stuff

--- Scripts/Inventory/PickUp.gd
+++ Scripts/Inventory/PickUp.gd
@@ -1,8 +1,9 @@
-extends StaticBody2D
+extends Area2D
 @export var item: InvItem
 var player = null 
 
-func _on_area_2d_body_entered(body):
+func _on_body_entered(body):
+	print("I work!")
 	player = body
 	PlayerCollect()
 	await  get_tree().create_timer(0.1).timeout
