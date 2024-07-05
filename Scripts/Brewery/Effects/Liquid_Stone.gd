96e2cf5bf1b8d54b364a30409a264fdd2f04e51a
Justwouter <wouter2312@gmail.com>
2024-07-05 19:35:50 +0200

    liquid stone fix

--- Scripts/Brewery/Effects/Liquid_Stone.gd
+++ Scripts/Brewery/Effects/Liquid_Stone.gd
@@ -4,19 +4,18 @@ extends PotionEffect
 var player: PlayerActor
 var visionsprite: TextureRect
 
-func on_activate(_caller: Node):
+func on_activate(_caller: Node) -> void:
+	
 	player = _caller.get_parent() as PlayerActor
 	visionsprite = player.get_node("Camera2D/limitedvision")
-	if (visionsprite):
+	
+func _update(_caller: Node, _delta: float) -> void:
+	if (visionsprite&&player):
 		visionsprite.visible = true
 		player.health_system.invincible = true
 
-func _update(_caller: Node, _delta: float):
-	pass
 	
-func on_deactivate(_caller: Node):
+func on_deactivate(_caller: Node) -> void:
 	if (visionsprite):
 		visionsprite.visible = false
 		player.health_system.invincible = false
-
-		
